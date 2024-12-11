6c4ea952c95a9df3c8b9e2b58ee1e562ebd72301
Anushka Gupta <anushkayol001@gmail.com>
2024-12-11 22:24:19 +0530

    made changes to root directory page for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  assetPrefix: '/',
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/index',
+      }
+    ];
+  }
+};
 
 export default nextConfig;
