3c64ca49e83f5b0b5ddd3746c8281a9af9263dd3
Anushka Gupta <anushkayol001@gmail.com>
2024-12-15 00:22:07 +0530

    reverted changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true, // Ensure app directory is enabled
-  },
   images: {
     unoptimized: true,
   },
